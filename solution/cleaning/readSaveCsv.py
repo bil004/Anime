eb239a27a57fc9c309a928cb590cdb0762793daf
Bilal <bilal.benslimane@di.unito.it>
2026-01-10 11:58:10 +0100

    added files

--- solution/cleaning/readSaveCsv.py
+++ solution/cleaning/readSaveCsv.py
@@ -1,13 +1,26 @@
 import pandas as pd
+import duckdb as dd
 import os
 
+# Usare chunk + Dtypes per ottimizzare la lettura
+
 def load_csv_file(file):
-    df = pd.read_csv(file)
-    return df
+    return pd.read_csv(file)
 
 def save_csv_file(df, file):
+    results = []
     if not os.path.exists('../../cleaned_data'):
         os.makedirs('../../cleaned_data')
 
-    df.to_csv(file, index=False)
+
+    df.to_csv(file)
     print(f"DataFrame saved to {file}")
+
+
+
+def query_big_files(file, query):
+    con = dd.connect("../../cleaned_data/big_data.db")
+    res = con.execute(query).df()
+    print(res)
+
+    return res
