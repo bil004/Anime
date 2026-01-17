e7e2cd08f256b9026a5802e6e87d96854c7db6f7
Luca Dalla Villa <luca.dallavilla@edu.unito.it>
2026-01-17 18:05:40 +0100

    fixed read/save csv function calls

--- solution/dbconnection.py
+++ solution/dbconnection.py
@@ -1,8 +1,10 @@
-import pandas as pd
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from pathlib import Path
 import os
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # CONFIGURAZIONE
 db_config = {
@@ -13,7 +15,7 @@ db_config = {
     'database': ''
 }
 
-def load_config(path='.env'):
+def load_config(path=BASE_DIR / '.env'):
     load_dotenv(path)
     db_config['user'] = os.getenv('DB_USER')
     db_config['password'] = os.getenv('DB_PASS')
@@ -21,8 +23,11 @@ def load_config(path='.env'):
     db_config['port'] = os.getenv('DB_PORT')
     db_config['database'] = os.getenv('DB_NAME')
 
+    # for key, value in db_config.items():
+    #     print(f"{key}: {value}")
 
-def create_db_engine(path='.env'):
+
+def create_db_engine(path=BASE_DIR / '.env'):
     load_config(path)
     db_url = f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
     return create_engine(db_url)
